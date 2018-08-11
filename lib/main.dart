cb4a275e1214bf68c85b6f6f4fadfd0784fef9af
fabiuz <ccmaismais@yahoo.com>
2018-08-11 10:14:59 -0300

    Primeiro commit.

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyStatelessWidget(),
+  ));
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Stateless Widget")),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MyCard(
+              title: Text("I love flutter", style: TextStyle(fontSize: 25.0)),
+              icon: Icon(Icons.favorite, size: 40.0, color: Colors.redAccent),
+            ),
+            MyCard(
+              title: Text("I love donuts", style: TextStyle(fontSize: 25.0)),
+              icon: Icon(Icons.donut_large, size: 40.0, color: Colors.brown),
+            ),
+            MyCard(
+              title: Text("I see you", style: TextStyle(fontSize: 25.0)),
+              icon: Icon(Icons.visibility, size: 40.0, color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  MyCard({this.title, this.icon});
+
+  final Widget title;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: new EdgeInsets.only(bottom: 20.0),
+        child: Card(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                this.title,
+                this.icon,
+              ],
+            ),
+          ),
+        ));
+  }
+}
